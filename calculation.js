85c127aa870ffa0951512c65bfa91cad1e7c3cae
Igor Salviatto <igorschsal@gmail.com>
2021-11-04 17:07:28 -0300

    fecth data api and fix date functions

--- calculation.js
+++ calculation.js
@@ -16,3 +16,20 @@ var month = currentTime.getMonth();
 const axios = require('axios');
 const token = process.env.NEXT_PUBLIC_BEARER_TOKEN;
 axios.defaults.headers.common = { 'Authorization': `bearer ${token}` };
+async function getIndex(url) {
+    const { data } = await axios.get(url);
+    // TODO: Guardar indices em um arquivo json dentro do projeto (atualizar mensalmente)
+    return data;
+}
+function fixHyphenDate(time) {
+    if (time.includes('-')) {
+        const newDate = `${time.split('-')[2]}/${time.split('-')[1]}/${time.split('-')[0]}`
+        return newDate
+    }
+}
+function returnHyphenDate(time) {
+    if (time.includes('/')) {
+        const newDate = `${time.split('/')[2]}-${time.split('/')[1]}-${time.split('/')[0]}`
+        return newDate
+    }
+}
