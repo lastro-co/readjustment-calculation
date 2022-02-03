44d9a28c553b18417c4a9fa90d13ba1e76e2e9ef
Igor Salviatto <igorschsal@gmail.com>
2022-02-03 01:45:27 -0300

    fix: minor bugs

--- calculation.js
+++ calculation.js
@@ -51,20 +51,22 @@ async function Calculation({ index, baseDate, baseValue }) {
     const indicePrimeiro = readjusmentData.map(function (e) { return e.data; }).indexOf(initialDate);
 
     calcDataStart.push(readjusmentData.slice(indicePrimeiro));
-    const mesultimoindice = calcDataStart[0][(calcDataStart[0].length - 1)].data.split('/')[1] == 12 ? 1 : calcDataStart[0][(calcDataStart[0].length - 1)].data.split('/')[1] + 1
+    const mesultimoindiceaux = parseInt(calcDataStart[0][(calcDataStart[0].length - 1)].data.split('/')[1]) == 12 ? '01' : parseInt(calcDataStart[0][(calcDataStart[0].length - 1)].data.split('/')[1]) + 1
+    const mesultimoindice = `${mesultimoindiceaux}`.length > 1 ? mesultimoindiceaux : `0${mesultimoindiceaux}`
+
     //se mes da basedate >= ultimo indice+1 -> pega ultimo ano por ultimo e avisa dados indisp
     if (initialDate.split('/')[1] > mesultimoindice) {
         const dateReajust = `01/${initialDate.slice(3, 5)}/${year - 1}`;
         const indiceUlitmo = calcDataStart[0].map(function (e) { return e.data; }).indexOf(dateReajust);
         calcDataFinal.push(calcDataStart[0].slice(0, indiceUlitmo));
         dateReajustment.push(returnHyphenDate(dateReajust))
-        console.log(dateReajust)
 
-        // se mesbase menor q mes ultimo indice, pega ate 2021
+        // se mesbase menor q mes ultimo indice, pega ate ano
     } else {
         const dateReajust = `01/${initialDate.slice(3, 5)}/${year}`;
-        const dateJanuary = `01/12/${year - 1}`
-        const indiceUlitmo = calcDataStart[0].map(function (e) { return e.data; }).indexOf(initialDate.slice(3, 5) == '01' ? dateJanuary : dateReajust);
+        const monthIndex = `${initialDate.slice(3, 5) - 1}`.length > 1 ? (initialDate.slice(3, 5) - 1) : `0${initialDate.slice(3, 5) - 1}`
+        const dateLastIndex = initialDate.slice(3, 5) == '01' ? `01/12/${year - 1}` : `01/${monthIndex}/${year}`
+        const indiceUlitmo = calcDataStart[0].map(function (e) { return e.data; }).indexOf(dateLastIndex);
         calcDataFinal.push(calcDataStart[0].slice(0, indiceUlitmo + 1));
         dateReajustment.push(returnHyphenDate(dateReajust))
 
@@ -77,8 +79,10 @@ async function Calculation({ index, baseDate, baseValue }) {
         acc = acc * (1 + percentage)
         //salva a memoria de calculo anual
         if ((item) % 12 == 11) {
+            console.log(calcDataFinal[0][item].data.slice(6))
+
             calcMemory.push({
-                date: (`${mesultimoindice == "01" ? (parseInt(calcDataFinal[0][item].data.slice(6)) + 1) : calcDataFinal[0][item].data.slice(6)}-${initialDate.slice(3, 5)}`),
+                date: (`${initialDate.slice(3, 5) == "01" ? (parseInt(calcDataFinal[0][item].data.slice(6))) + 1 : calcDataFinal[0][item].data.slice(6)}-${initialDate.slice(3, 5)}`),
                 value: rentFinal.toFixed(2).replace('.', ','),
                 rate: `${((acc - 1) * 100).toFixed(2).replace('.', ',')}`,
             })
@@ -102,20 +106,20 @@ async function Calculation({ index, baseDate, baseValue }) {
 }
 
 // cénario de uso
-/** 
+
 (async (calculation) => {
     // IIFE
     try {
         const resultIGPM = await calculation({
             index: 'IGPM',
-            baseDate: '2018-01-01',
+            baseDate: '2019-07-01',
             baseValue: 'R$ 0,36',
         })
         console.log(resultIGPM) // { value: 363.17, memory: []
 
         const resultIPCA = await calculation({
             index: 'IPCA',
-            baseDate: '2019-08-01',
+            baseDate: '2019-01-01',
             baseValue: 'R$ 1.000.000,33',
         })
         console.log(resultIPCA) // { value: 370.25, memory: [] }
@@ -124,6 +128,6 @@ async function Calculation({ index, baseDate, baseValue }) {
         console.error(error) // new Error();
     }
 })(Calculation)
-*/
+
 
 exports.Calculation = Calculation;
